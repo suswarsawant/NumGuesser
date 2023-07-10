05da460a9bf3ebfbcec90af24c25daf8b1cf88c7
Suswar Sawant <Suswar.Sawant@asl.mum.sst>
2023-07-10 23:46:52 +0530

    Last commit

--- NumGuess.java
+++ NumGuess.java
@@ -1,38 +1,38 @@
 
+import java.util.Random;
 import java.util.Scanner;
 public class NumGuess {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int min=1;
-		int max=100;
-		int randomNumber=(int)(Math.random()*(max-min+1)+min);
-		int attempts=1;
+		Scanner sc = new Scanner(System.in);
+		Random num = new Random();
+		int randomNumber = num.nextInt(100);
+		System.out.println(randomNumber);
+		int attempts = 1;
 		System.out.print("Enter a number between 1 to 100 : ");
-		int number=sc.nextInt();
-		if(number>0 && number<=100) {
-			while(attempts<7){
-				
-				if(number<randomNumber) {
+		int number = sc.nextInt();
+		if (number > 0 && number <= 100) {
+			while (attempts < 6) {
+
+				if (number < randomNumber) {
 					System.out.print("Number is low, enter a higher number : ");
-					number=sc.nextInt();
-				}
-				else if(number>randomNumber) {
+					number = sc.nextInt();
+				} else if (number > randomNumber) {
 					System.out.print("Number is high, enter a lower number : ");
-					number=sc.nextInt();
-				}
-				else if(number==randomNumber){
-					System.out.println("You guessed correctly in "+attempts+" attempts!");
+					number = sc.nextInt();
+				} else if (number == randomNumber) {
+					System.out.println("You guessed correctly in " + attempts + " attempts!");
 					break;
 				}
-				attempts+=1;
-				if(attempts==7 && number!=randomNumber) {
+				attempts += 1;
+				if (attempts == 6 && number != randomNumber) {
 					System.out.println("NUMBER OF ATTEMPTS EXHAUSTED");
+				} else if(attempts == 6 && number == randomNumber){
+					System.out.println("You guessed correctly in " + attempts + " attempts!");
 				}
-			
+
 			}
-		}
-		else {
+		} else {
 			System.out.println("INVALID INPUT");
 		}
 			
